5838a83ad13be4416f492fec41a95e324302a083
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-06-01 17:48:16 -0300

    se creo componente header y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css"
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-      Hola mundo
-    </div>
+    <>
+     <Header></Header>
+     <main className="mainPage">
+
+     </main>
+     <Footer></Footer>
+    </>
   );
 };
 
